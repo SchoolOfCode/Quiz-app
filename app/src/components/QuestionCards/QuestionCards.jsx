9a7f773dc697683162eeb8b0eb3a5fb6a2e45bd5
rw4n99 <rowangodfrey2@gmail.com>
2024-08-29 21:19:29 +0100

    Styled quiz form

--- app/src/components/QuestionCards/QuestionCards.jsx
+++ app/src/components/QuestionCards/QuestionCards.jsx
@@ -4,33 +4,42 @@ import styles from './questionCards.module.css'
 export default function QuestionCards () {
     return (
     <div className={styles.wrapper}>
-        <form className={styles.form}>
+        <div className={styles.form}>
             <legend className={styles.title}>Question 1</legend>
-                <input
-                    type='radio'
-                    id='Answer1'
-                    name="Answer1"
-                    />
-                    <label className={styles.text} htmlFor='Answer1'>Answer 1</label>
+            <div className={styles.answerGroup}>
                 <input 
-                    type='radio'
-                    id='Answer2'
-                    name="Answer2"
-                    />
-                    <label className={styles.text} htmlFor='Answer2'>Answer 2</label>
-                <input
-                    type='radio'
-                    id='Answer3'
-                    name="Answer3"
-                    />
-                    <label className={styles.text} htmlFor='Answer3'>Answer 3</label>
-                <input
-                    type='radio'
-                    id='Answer4'
-                    name="Answer4"
-                    />
-                    <label className={styles.text} htmlFor='Answer4'>Answer 4</label>
-        </form>
+                    type="radio" 
+                    id="Answer1" 
+                    name="Answer" />
+                <label className={styles.text} htmlFor="Answer1">Answer 1</label>
+            </div>
+            <div className={styles.answerGroup}>
+                <input 
+                    type="radio" 
+                    id="Answer2" 
+                    name="Answer" />
+                <label className={styles.text} htmlFor="Answer2">Answer 2</label>
+            </div>
+            <div className={styles.answerGroup}>
+                <input 
+                    type="radio" 
+                    id="Answer3" 
+                    name="Answer" />
+                <label className={styles.text} htmlFor="Answer3">Answer 3</label>
+            </div>
+            <div className={styles.answerGroup}>
+                <input 
+                    type="radio" 
+                    id="Answer4" 
+                    name="Answer" />
+                <label className={styles.text} htmlFor="Answer4">Answer 4</label>
+            </div>
+            <button 
+                className={styles.button} 
+                type="submit">
+                    Submit answer
+            </button>
+        </div>
     </div>
-    )
+)
 }
